b53f96bc9856db480006812a68da30cedbccaa88
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-07-22 12:33:17 +0100

    made changes: ''

--- composables/useFirebaseServer.js
+++ composables/useFirebaseServer.js
@@ -4,11 +4,6 @@ import { getFirestore } from 'firebase-admin/firestore';
 // Parse the service account JSON from the environment variable
 let firebaseServerAccounts = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
-firebaseServerAccounts = JSON.stringify(firebaseServerAccounts)
-  .replace(/\\/g, '\\\\')  // Escape backslashes
-  .replace(/"/g, '\\"')    // Escape double quotes
-  .replace(/\n/g, '\\n');  // Escape new lines
-
 let app;
 if (getApps().length === 0) {
   app = initializeApp({
