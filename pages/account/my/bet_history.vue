b55944bfff5670f071c67447dfbde6b293788291
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-07-26 18:20:49 +0100

    made changes: ''

--- pages/account/my/bet_history.vue
+++ pages/account/my/bet_history.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="block space-y-4 lg:space-y-0 lg:flex gap-4 lg:max-w-[80%] mx-auto px-4 sm:px-8 md:px-8 lg:px-18 my-8">
     <AccountNav />
-    <div class="flex flex-col items-start justify-start gap-4 border shadow-md rounded-md w-full px-4 py-4">
-      <AccountHistory />
-      <AccountHistoryData />
+    <div class="grid grid-cols-4 items-start justify-start gap-4 border shadow-md rounded-md w-full px-4 py-4">
+      <AccountHistory class="col-span-1" />
+      <AccountHistoryData class="col-span-3" />
     </div>
   </div>
 </template>
