cb1afb42f79342f8e836af570245eec8a00f946d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-07-26 18:39:48 +0100

    made changes: ''

---
+++ components/Account/Issue/Create.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Create new issue</h1>
+    <UForm  class="space-y-3">
+      <UFormGroup label="Full name" name="fullName">
+        <UInput  placeholder="Management Information Systems FC" />
+      </UFormGroup>
+      <UFormGroup label="Short name" name="shortName">
+        <UInput  placeholder="MIS" />
+      </UFormGroup>
+      
+    <UButton  type="submit" color="black">
+      Submit
+    </UButton>
+  </UForm>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/account/my/support.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block space-y-4 lg:space-y-0 lg:flex gap-4 lg:max-w-[80%] mx-auto px-4 sm:px-8 md:px-8 lg:px-18 my-8">
+    <AccountNav />
+    <div class="block lg:grid grid-cols-2 items-start justify-start gap-4 border shadow-md rounded-md w-full px-4 py-4">
+      <AccountIssueList />
+      <AccountIssueCreate />
+    </div>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  middleware: ['auth']
+});
+
+const route = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
