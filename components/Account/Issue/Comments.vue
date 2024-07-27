266fcdfb0c6e9be1bcead31962769e2f5515eb87
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-07-27 23:31:29 +0100

    made changes: ''

---
+++ components/Account/Issue/Comments.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Comments on - {{ route.query.issue }}
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Account/Issue/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ route.query.issue }}
+    <NuxtLink to="/account/my/support" >Back</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
