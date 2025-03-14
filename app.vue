e29f28ec9eac61230a3412c7e431949accd05786
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-03-14 12:53:30 +0100

    activated the site

--- app.vue
+++ app.vue
@@ -2,41 +2,16 @@
   <div>
     <AppNavBar />
     <AppNavMBar />
-    <!-- <div class="max-w-[70%] mx-auto my-4 px-4">
-      <UNotification 
-      :ui="{
-        background: 'bg-yellow-400 dark:bg-yellow-700'
-      }"
-      v-if="userStatus === 'frozen'"
-      title="Your account has been frozen. Please contact customer support to identify and resolve any pending issues." 
-      :id="1" 
-      :timeout="0" 
-      :close-button="{disabled: true}" 
-      />
-      <UNotification 
-      :ui="{
-        background: 'bg-red-500 dark:bg-red-900'
-      }"
-      v-if="userStatus === 'suspended'"
-      title="Your account has been suspended. Please contact customer support to obtain more information regarding the suspension." 
-      :id="1" 
-      :timeout="0" 
-      :close-button="{disabled: true}" 
-      />
-    </div> -->
+    
     <main class="">
       <NuxtLoadingIndicator color="#76dbff" :height="4" />
-      <!-- <NuxtPage  /> -->
-      <div class="max-w-[80%] space-y-4 px-4">
+      <NuxtPage  />
+      <!-- <div class="max-w-[80%] space-y-4 px-4 py-4">
         <p>We wish to inform you that BettonyX will be temporarily suspending all operations effective immediately as we work diligently to secure both a betting license and a Mobile Money Operator (MMO) license. These licenses are crucial for compliance with regulatory requirements, and without them, we are unable to process deposits through our payment provider, Paystack.</p>
         <p>We deeply appreciate your continued support and understanding during this period. Please rest assured that we are committed to resuming operations as swiftly as possible. You will be promptly notified via email as soon as we are able to reinstate payment services.</p>
         <p>Thank you for your patience and loyalty to BettonyX.</p>
-      </div>
+      </div> -->
       
-
-
-
-
     </main>
     <UNotifications />
     <AppFooterAttribution />
@@ -77,3 +52,25 @@ const userStore = useUserStore();
 const userStatus = ref(userStore.getUserData.status);
 
 </script>
+<!-- <div class="max-w-[70%] mx-auto my-4 px-4">
+      <UNotification 
+      :ui="{
+        background: 'bg-yellow-400 dark:bg-yellow-700'
+      }"
+      v-if="userStatus === 'frozen'"
+      title="Your account has been frozen. Please contact customer support to identify and resolve any pending issues." 
+      :id="1" 
+      :timeout="0" 
+      :close-button="{disabled: true}" 
+      />
+      <UNotification 
+      :ui="{
+        background: 'bg-red-500 dark:bg-red-900'
+      }"
+      v-if="userStatus === 'suspended'"
+      title="Your account has been suspended. Please contact customer support to obtain more information regarding the suspension." 
+      :id="1" 
+      :timeout="0" 
+      :close-button="{disabled: true}" 
+      />
+    </div> -->
